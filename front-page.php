d60446988708de3534053ef5eb48dc6eab0494f6
Jayeson Brewster <jayeson.brewster@gmail.com>
2021-01-29 16:31:04 -0500

    first commit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php /* 
+This page is used to display the static frontpage. 
+*/
+ 
+// Fetch theme header template
+get_header(); ?>
+    <div id="primary">
+
+<?php echo do_shortcode("[pt_view id=d58ca5cm07]"); ?>
+
+<a href="https://www.thefactsite.com/">The Fact Site</a> - thank you for supplying a lot of the facts for this site.</a>
+
+    </div><!-- #primary -->
